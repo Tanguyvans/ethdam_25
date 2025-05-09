aac86636f79aea86ec5d173aee5888974dacb248
Tanguy VANSNICK <191451@umons.ac.be>
2025-05-10 01:24:56 +0200

    trying

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // This is critical
+  images: {
+    unoptimized: true,
+  },
+  // any other configurations
 };
 
 export default nextConfig;
