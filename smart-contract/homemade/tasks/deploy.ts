fe66aa0250783c46b70feb7e3a768a97094536f8
Rodrigue Deghorain <rodriguedeghorain@hotmail.be>
2025-05-09 16:45:16 +0200

    Deploy smart contract

---
+++ smart-contract/homemade/tasks/deploy.ts
@@ -0,0 +1,15 @@
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+task('deploy', 'Deploys our contract')
+    .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
+        const factory = await hre.ethers.getContractFactory('ChallengeManager');
+        const contract = await factory.deploy();
+
+        const dt = contract.deploymentTransaction();
+        console.log('Deployment Transaction', dt!.hash);
+
+        await contract.waitForDeployment();
+
+        console.log('Contract', await contract.getAddress());
+    });
